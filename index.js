189b100b5014983eb86ce40bae8d5af461499b44
Burak Karaman <karaman.buraak@gmail.com>
2024-07-06 16:27:12 +0200

    Add configurations, middleware, and routes to index.js; create Room model and schema; and update roomController.js to require Room model.

--- index.js
+++ index.js
@@ -7,13 +7,37 @@ const express = require("express");
 const app = express();
 
 /* ------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                               CONFIGURATIONS                             */
+/* -------------------------------------------------------------------------- */
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
 
+require("express-async-errors");
+
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
+
+/* -------------------------------------------------------------------------- */
+/*                               MIDDLEWARES                                  */
+/* -------------------------------------------------------------------------- */
+app.use(express.json());
+
+require("./src/middlewares/findSearchSortPagi");
+
+/* -------------------------------------------------------------------------- */
+/*                               ROUTES                                       */
+/* -------------------------------------------------------------------------- */
+
+app.all("/", (req, res) => {
+  res.send("<h1>Welcome to the Hotel API</h1>");
+});
 /* ------------------------------------------------------- */
 
 // errorHandler:
 app.use(require("./src/middlewares/errorHandler"));
 
 // RUN SERVER:
-app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
 
 /* ------------------------------------------------------- */
