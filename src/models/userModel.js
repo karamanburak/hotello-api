e66429b070b36c5add8c173b9f446753e5204562
Burak Karaman <karaman.buraak@gmail.com>
2024-07-06 17:52:42 +0200

    Add User model with schema and update roomRouter with idValidation middleware

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,3 +4,43 @@
 ------------------------------------------------------- */
 const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema = ({
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        minLength: 8,
+        // select:false
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        // validate: [
+        //     (email) => email.includes("@") && email.split("@")[1].includes("."),
+        //     "Email is invalid!",
+        // ],
+        validate:[validator.isEmail, "Please provide a valid email"]
+    },
+    isActive:{
+        type:Boolean,
+        default:true
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    }
+}, {
+    collection:"user",
+    timestamps:true
+})
+
+module.exports = mongoose.model("UserModel", UserSchema)
--- src/routes/roomRouter.js
+++ src/routes/roomRouter.js
@@ -5,10 +5,12 @@
 const router = require("express").Router();
 /* ------------------------------------------------------- */
 const room = require("../controllers/roomController");
+const idValidation = require("../middlewares/idValidation");
 
 router.route("/").get(room.list).post(room.create);
 router
   .route("/:id")
+  .all(idValidation)
   .get(room.read)
   .put(room.update)
   .patch(room.update)
