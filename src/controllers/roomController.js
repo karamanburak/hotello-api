6896404c684cbba8d4508a992b2c9dc7f6fe08d2
Burak Karaman <karaman.buraak@gmail.com>
2024-07-06 16:53:22 +0200

    Add CRUD operations to roomController.js

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -3,3 +3,46 @@
     EXPRESS - HOTEL API
 ------------------------------------------------------- */
 const Room = require("../models/roomModel");
+
+module.exports = {
+  list: async (req, res) => {
+    const rooms = await res.getModelList(Room);
+    res.status(200).send({
+      error: false,
+      detail: await res.getModelListDetails(Room),
+      data: rooms,
+    });
+  },
+  create: async (req, res) => {
+    const newRoom = await Room.create(req.body);
+    res.status(201).send({
+      error: false,
+      data: newRoom,
+    });
+  },
+  read: async (req, res) => {
+    // const rooms = await Room.findOne({_id:req.params.id})
+    const room = await Room.findById(req.params.id); // mongoose arka planda findOne methodunu calistirir.
+    res.status(200).send({
+      error: false,
+      data: room,
+    });
+  },
+  update: async (req, res) => {
+    const room = await Room.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
+    res.status(202).send({
+      error: false,
+      data: room,
+      newRoom: await Room.findOne({ _id: req.params.id }),
+    });
+  },
+  delete: async (req, res) => {
+    const room = await Room.deleteOne({ _id: req.params.id });
+    res.status(data.deletedCount > 0 ? 204 : 404).send({
+      error: !data.deletedCount,
+      data: room,
+    });
+  },
+};
